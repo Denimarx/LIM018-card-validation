79dc05105e357719bf901b9c2b01814a55967f7b
Denimarx <denimar2022@gmail.comm>
2022-06-02 00:57:51 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import validator from './validator.js';
 
-console.log(validator);
+console.log(validator.isValid());
+
+  /*
+1. traer la funcion de validator
+2. si es true lanzar retur SU TARJETA ES VALIDA
+--sino es INVALIDA
+3.boton validar
+4.boton limpiar o probar otra tarjeta
+*/
+/*
+function isValid() {
+  const isValid = validator.isValid("cardnumber");
+const cardnumber = document.getElementById("cardnumber");
+const btnvalidate = document.getElementById('btnvalidate');
+let message = '';
+
+if (validator.isValid(carsnumber.value) === true)
+message = "tarjeta valida";
+  
+else
+message = "tarjeta invalida";
+} 
+*/
+
+
+const isValid = validator.isValid('cardnumber');
+
+const cardnumberinput = document.getElementById('cardnumber');
+const botonvalidate = document.getElementById('btnvalidate');
+let message = '';
+
+btn.validate.addEventListener ("click",(e)) ; {e.preventDefault();
+  if (isValid === true) {
+    message = "Tarjeta Válida"
+  } else {
+    message = "Tarjeta Inválida"
+  }
+}
+
+$formName.addEventListener("submit", handleSubmitName);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,52 @@
 const validator = {
-  // ...
-};
+
+/*
+1.capturar el cardnumber ingresado ( signo michi para los primeros numeros menos los 4 ultimos)
+--no acepta espacios ni letras
+-- debe ingresar nombre, numero de tarjeta  (cvv y vto)
+2.invertir los digitos
+3.posiciones pares multiplicarlos por 2
+4. las sumas >10 restarle 9 ( o sumar ambos digitos)
+5. sumar los numeros de posiciones impares y pares (resultado de restarle 9)
+6. suma %10 ===0 tarjeta es valida, sino es INVALIDA
+*/
+
+
+  isValid: function (cardnumber) {  
+    let CCNumber = cardnumber.split('');  
+    let numreverse = CCNumber.reverse();
+    let numpares = 0;
+    let numimpar = 0;
+
+    for ( let i = 1; i<CCNumber.length; i=i+2) {
+      let parpordos = numreverse[i]*2;
+      
+         if (parpordos >= 10) {
+           numpares = numpares + parpordos-9
+          } else {
+            numpares = numpares + parpordos
+          }
+          
+        }
+    for (let i=0; i<numreverse.length; i=i+2) {
+        numimpar += parseInt(numreverse[i])
+    }
+    let ifItsValid = (numimpar + numpares)%10
+    if (ifItsValid ===0) {
+      return true
+    } else {
+      return false
+    }
+
+  }
+   
+  /*maskify: function  {
+
+  }*/
+}
+
+  
+
 
 export default validator;
+
